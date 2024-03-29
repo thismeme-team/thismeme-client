3fdf8eae3e58914e78c01a9606adfb55c14580a0
Jeongjin Oh <rojay.developer@gmail.com>
2024-03-29 19:15:54 +0800

    improve: step2 of image optimization (#101)

--- src/api/search/useGetMemesByTag.ts
+++ src/api/search/useGetMemesByTag.ts
@@ -3,14 +3,12 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 
 import { api } from "../core";
 
+const FIRST_PAGE_SIZE = 10;
 const PAGE_SIZE = 20;
 
 /**
  * tag 밈 검색 API
  * @param tag 검색할 tag
- * @returns
- * data - 밈 검색 결과
- * isEmpty - 밈 검색 결과가 없는 경우 true
  */
 export const useGetMemesByTag = (tag: string) => {
   const {
@@ -18,20 +16,39 @@ export const useGetMemesByTag = (tag: string) => {
     isFetchingNextPage,
     isError,
     fetchNextPage: originalFetchNextPage,
-  } = useInfiniteQuery(
-    useGetMemesByTag.queryKey(tag),
-    ({ pageParam = 0 }: QueryFunctionContext) =>
-      api.search.getMemesByTag({ keyword: tag, offset: pageParam, limit: PAGE_SIZE }),
-    {
-      getNextPageParam: (lastPage, allPages) => {
-        const { count } = lastPage;
-        const isLastPage = count < PAGE_SIZE;
-        const offset = count * (allPages.length - 1);
-        return isLastPage ? undefined : offset + PAGE_SIZE;
-      },
-      enabled: !!tag,
+  } = useInfiniteQuery({
+    queryKey: useGetMemesByTag.queryKey(tag),
+    queryFn: ({ pageParam = 0 }: QueryFunctionContext) =>
+      api.search.getMemesByTag({
+        keyword: tag,
+        offset: pageParam,
+        limit: pageParam === 0 ? FIRST_PAGE_SIZE : PAGE_SIZE,
+      }),
+    select: (data) => {
+      return {
+        ...data,
+        pages: data.pages.map((page, index) => {
+          const isFirstPage = index === 0;
+          return {
+            ...page,
+            memes: page.memes.map((meme, index) => ({
+              ...meme,
+              priority: isFirstPage && index < 4,
+            })),
+          };
+        }),
+      };
     },
-  );
+    getNextPageParam: (lastPage, allPages) => {
+      const { count } = lastPage;
+      const isFirstPage = allPages.length === 1;
+      const pageSize = isFirstPage ? FIRST_PAGE_SIZE : PAGE_SIZE;
+      const isLastPage = count < pageSize;
+      const offset = count * (allPages.length - 1);
+      return isLastPage ? undefined : offset + pageSize;
+    },
+    enabled: !!tag,
+  });
 
   const fetchNextPage = isError
     ? ((() => {}) as typeof originalFetchNextPage)
